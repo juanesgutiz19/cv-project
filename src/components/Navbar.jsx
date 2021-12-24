800d001e8aa7a085f534c5abed74577ecb6d978f
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-12-24 11:45:11 -0500

    Se modifica el ancho del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     <div className="sticky top-0 z-50">
       <nav className="bg-slate-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center justify-between h-20">
             <div className="flex items-center">
                 <img
                   className="w-40"
