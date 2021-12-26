9d4a10b8352ba5f57696e191bb90ffd6e8fbe667
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-12-26 02:49:49 -0500

    último detalle de Academic

--- src/components/Academic.jsx
+++ src/components/Academic.jsx
@@ -6,7 +6,6 @@ const Academic = () => {
         <>
             <div className="">
                 <h1 className="text-gray-900 text-4xl md:text-5xl lg:text-7xl font-bold mt-10 text-center md:text-left md:mx-6 lg:mx-8" >Academic</h1>
-                {/* px-24 */}
                 <div className= "flex flex-col lg:flex-row px-10 md:px-24">
                     <AcademicCard qualification="High School Graduate" institution="Liceo Salazar y Herrera" period= { 2016 }/>
                     <AcademicCard qualification="Systems Engineering" institution="University of Antioquia"/>
