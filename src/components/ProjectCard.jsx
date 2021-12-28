a1829d9231b8732b42353f5e48095d16833d5e92
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-12-28 00:00:04 -0500

    Se termina la sección de Projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const ProjectCard = ( { picture, projectName, technologies, projectLink} ) => {
     return (
-        <div className="overflow-hidden shadow-2xl bg-white m-4">
+        <div className="overflow-hidden shadow-2xl rounded-sm bg-white">
             <img className="w-full border-2 border-gray-200" src={ picture }  alt="Sunset in the mountains"/>
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{ projectName }</div>
-                <p className="text-gray-700 text-base">{ technologies }</p>
+                <div className="font-bold text-2xl mb-1 text-gray-900">{ projectName }</div>
+                <p className="font-normal text-base text-gray-800">{ technologies }</p>
             </div>
-            <div className="px-6 pt-4 pb-2 text-center">
-                <a href={ projectLink } target="_blank" rel="noopener noreferrer" className=" hover:underline"><span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">View project</span></a>
+            <div className="px-6 pt-4 pb-4 text-center">
+                <a href={ projectLink } target="_blank" rel="noopener noreferrer" className=" hover:underline hover:decoration-white"><span className="bg-gray-700 rounded-full px-5 py-1 text-base font-normal text-white mr-2 mb-2">View project</span></a>
             </div>
         </div>
     )
